52e2c43751deb40b0f5999bcd530c9176a258de3
taesikk <dlwhsktm@gmail.com>
2026-05-25 20:31:59 +0900

    #11 corsConfig 주석 처리

--- springboot-backend/src/main/java/com/AutoTradingWithDocker/config/CorsConfig.java
+++ springboot-backend/src/main/java/com/AutoTradingWithDocker/config/CorsConfig.java
@@ -4,17 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://taesikk.duckdns.org:3000",  // 프론트 주소
-                        "http://localhost:3000"               // 로컬 개발용
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins(
+//                        "http://taesikk.duckdns.org:3000",  // 프론트 주소
+//                        "http://localhost:3000"               // 로컬 개발용
+//                )
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+//}
